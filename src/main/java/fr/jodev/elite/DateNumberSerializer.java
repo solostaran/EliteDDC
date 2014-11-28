64f40847031d2914864e8b31afeeea9db7043420
JRD <solostaran14@gmail.com>
2014-11-28 17:00:56 +0100

    Added Long to Date Serializer (number of millisec since January 1st 1970) See also the Goods entity.

---
+++ src/main/java/fr/jodev/elite/DateNumberSerializer.java
@@ -0,0 +1,26 @@
+package fr.jodev.elite;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class DateNumberSerializer extends StdSerializer<Long> {
+	
+	public DateNumberSerializer() {
+		super(Long.class);
+	}
+
+	@Override
+	public void serialize(Long date, JsonGenerator json, SerializerProvider provider)
+			throws IOException, JsonGenerationException {
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		String out = df.format(new Date(date));
+		json.writeString(out);
+	}
+}
--- src/test/java/fr/jodev/elite/tests/GenDate.java
+++ src/test/java/fr/jodev/elite/tests/GenDate.java
@@ -1,11 +1,16 @@
 package fr.jodev.elite.tests;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class GenDate {
 
 	public static void main(String[] args) {
 		System.out.println("["+new Date().getTime()+"]");
+		
+		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		System.out.println(df.format(new Date()));
 	}
 
 }
